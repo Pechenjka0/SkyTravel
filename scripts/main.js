da5ba38aa55015c4544ee373352f0048d5747d19
Pechenjka0 <hai.nik@mail.ru>
2025-05-09 22:41:31 +0500

    секция reviews

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
