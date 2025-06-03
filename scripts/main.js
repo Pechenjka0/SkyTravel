6539bd7ce1452044789c4383b9ea3416640d8b74
Pechenjka0 <hai.nik@mail.ru>
2025-06-03 16:28:19 +0500

    Код для генерации страниц из tours.json, уменьшил шрифт в таблице до 12px, добавил главные изображения для всех направлений

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,11 @@
 import initSlider from './slider.js';
 import initAccordion from './accordion.js';
 import initBurgerMenu from './burger.js';
+import { loadDestination } from './loadDestination.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     initSlider();
     initAccordion();
     initBurgerMenu();
+    loadDestination();
 });
